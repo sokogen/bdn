080bd28418bae3c21d001d7e765fe62a81fddcf3
Sokogen <g@sokolachko.com>
2017-01-24 18:03:30 +0300

    Start constructing daemon mode

--- bdn.py
+++ bdn.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#
 
 ### Configuration
 cfg={
 ### Настройка доступа к Confluence
 # URL страницы с таблицей:
 'cflurl': 'http://212.15.125.130:8090/pages/viewpage.action?pageId=65592#space-menu-link-content',
-'cfluser': 'admin',								# Логин для доступа к Confluence
-'cflpass': 'Telegraph!',						# Пароль для доступа к Confluence
+'cfluser': 'admin',				# Логин для доступа к Confluence
+'cflpass': 'Telegraph!',			# Пароль для доступа к Confluence
 
 
 ### Настройка формата данных таблицы
-'bdcol': 'День рождения',						# Заголовок колонки с датами дней рождений
-'emailcol': 'email',							# Заголовок колонки с адресами e-mail
-'firedstaffcol': 'Комната',						# Заголовок колонки для помечания уволенных сотрудников
-'firedstaffmarks': ('уволен','Уволен'),			# Метки для помечания уволенных сотрудников в колонке "Комната"
+'fnamecol': 'ФИО',				# Заголовок колонки с полным именем
+'bdcol': 'День рождения',			# Заголовок колонки с датами дней рождений
+'emailcol': 'email',				# Заголовок колонки с адресами e-mail
+'directioncol': 'Направление работы',		# Заголовок колонки с направлениями работы
+'firedstaffcol': 'Комната',			# Заголовок колонки для помечания уволенных сотрудников
+'firedstaffmarks': ('уволен','Уволен'),		# Метки для помечания уволенных сотрудников в колонке "Комната"
 
 ### Настройка оповещений
-'smtpserver': 'localhost',						# Адрес SMTP сервера
-'bdaysrange': (-1, 7),							# Количество дней "вокруг" дня рождения 
-'notificationtime': '09:00',					# Время рассылки оповещений
-'notificationweekdays': (1,2,3,4,5),			# Номера дней недели для оповещений
+'smtpserver': 'localhost',			# Адрес SMTP сервера
+'bdaysrange': (-1, 7),				# Количество дней "вокруг" дня рождения
+'notificationtime': '09:00',			# Время рассылки оповещений
+'notificationweekdays': (1,2,3,4,5),		# Номера дней недели для оповещений
+'mailsender': 'noreply@server',			# Отправитель сообщения в поле "From:"
+'mailsubject': 'Дни рождения',			# Тема письма ('Subject:')
 }
 
-import json, re
+# Лечение возможных проблем кодировок
+import setupcon
+setupcon.setup_console('utf-8', False)
+
+# Стандартные библиотеки
+import json, re, smtplib
 from urlparse import urlparse
 from datetime import datetime, timedelta
+from time import sleep
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# Внешние зависимости ()
 import requests
 from bs4 import BeautifulSoup
-
 
 class CflPageParser:
 	def __init__(self, cfg=cfg):
@@ -44,17 +54,16 @@ class CflPageParser:
 
 	def createresturl(self, cflurl):
 		# Берем стандартный URL и возвращаем REST URL.
-		self.cflhosturl='{}://{}'.format(urlparse(self.cflurl)[0], urlparse(self.cflurl)[1])
+		self.cflhosturl='{0}://{1}'.format(urlparse(self.cflurl)[0], urlparse(self.cflurl)[1])
 		self.pageid=int(urlparse(self.cflurl)[4].split("=")[1])
-		self.resturl='{}/rest/api/content/{}?expand=body.storage'.format(self.cflhosturl, self.pageid)
+		self.resturl='{0}/rest/api/content/{1}?expand=body.storage'.format(self.cflhosturl, self.pageid)
 		return self.resturl
 
 	def gettablesdict(self):
 		# Собираем все словари таблиц в одну коллекцию
 		self.createtablesdata(self.resturl)
-		## It must be rewritten
-		self.tablesdict = list(map(lambda table: self.createtabledict(table), self.tables))
-		self.tablesdict = tuple(reduce(lambda d, t: d.extend(t), self.tablesdict))
+		## It can be rewritten
+		self.tablesdict = tuple(reduce(lambda d, t: d.extend(t), map(lambda table: self.createtabledict(table), self.tables)))
 		return self.tablesdict
 
 	def createtablesdata(self, resturl):
@@ -64,7 +73,7 @@ class CflPageParser:
 		# Выбирем только содержание таблиц со страницы
 		self.tables = self.rawdata.findAll('table')
 		return self.tables
-	
+
 	def createtabledict(self, table):
 		# Выбираем заголовки колонок
 		self.headsdict = tuple(map(lambda h: h.get_text().strip(), table.findAll('th')))
@@ -77,38 +86,95 @@ class CflPageParser:
 	def getstaffdict(self):
 		return filter(lambda person: person[self.firedstaffcol].strip() not in self.firedstaffmarks, self.tablesdict)
 
-class BDNotice:
+class BDNotice():
+	"""
+	Класс получает данные от парсера таблиц Confluence,
+	формирует письмо с информацией о ближайших днях рождения сотрудников,
+	и отправляет его остальным.
+	"""
 	def __init__(self, cfg=cfg):
-		self.today = datetime.now()
-		self.marginlow = self.today + timedelta(days=int(cfg['bdaysrange'][0]))
-		self.marginhigh = self.today + timedelta(days=int(cfg['bdaysrange'][-1]))
+		self.bdstaffdict = {'bd':{},'recipients':[]}
 		self.bdcol = cfg['bdcol'].decode('utf-8')
-		self.staffdict = CflPageParser().getstaffdict()
-		print CflPageParser().getstaffdict()
-		self.createbdnoticedict(self.staffdict)
-		print self.checkbddate('22.01')
+		self.emailcol = cfg['emailcol'].encode('utf-8')
+		self.fnamecol = cfg['fnamecol'].decode('utf-8')
+		self.directioncol = cfg['directioncol'].decode('utf-8')
 
-	def createbdnoticedict(self, staffdict):
+		self.mailbody_temp = str(open('mail_template/mail_body.txt').read())
+		self.maildates_temp = str(open('mail_template/dates.txt').read())
+		self.mailperson_temp = str(open('mail_template/person.txt').read())
+
+	def run(self):
+		while True:
+			if self.checktime():
+				self.createbdnotice(CflPageParser().getstaffdict())
+			sleep(31)
+
+	def checktime(self):
+		# Проверяем день недели и время
+		self.today = datetime.now()
+		self.notificationtime = '{0} {1}'.format(self.today.strftime("%Y-%m-%d"), cfg['notificationtime'])
+		return self.today.strftime("%Y-%m-%d %H:%M") == self.notificationtime and self.today.isoweekday() in cfg['notificationweekdays']
+
+	def cronrun(self):
+		self.createbdnotice(CflPageParser().getstaffdict())
+
+	def createbdnotice(self, staffdict):
+		# Проверяем даты рождений у всех сотрудников.
+		# Именинников раскладываем по датам, остальных в список адресатов.
 		for person in staffdict:
-			person[self.bdcol]=self.checkbddate(person[self.bdcol])
-			print person[self.bdcol]
+			bddate = self.checkbddate(person[self.bdcol])
+			if bddate != None:
+				try:
+					self.bdstaffdict['bd'][bddate].append(person)
+				except KeyError:
+					self.bdstaffdict['bd'][bddate]=list()
+					self.bdstaffdict['bd'][bddate].append(person)
+			else:
+				self.bdstaffdict['recipients'].append(person[self.emailcol])
+
+		# Генерируем тело письма
+		self.dateslist = str()
+		for date in self.bdstaffdict['bd'].keys():
+			self.personslist = str()
+			for person in self.bdstaffdict['bd'][date]:
+				self.personslist+=(self.mailperson_temp.format(email=person[self.emailcol], name=person[self.fnamecol], direction=person[self.directioncol]))
+			self.dateslist+=self.maildates_temp.format(date=date, persons=self.personslist)
+		self.mailhtml=self.mailbody_temp.format(dates=self.dateslist)
+
+		# Отсылаем письмо адресатам
+		for rec in self.bdstaffdict['recipients']:
+			self.sendbdnotice(rec, self.mailhtml)
 
 
 	def checkbddate(self, bddate):
+		self.marginlow = self.today + timedelta(days=int(cfg['bdaysrange'][0]))
+		self.marginhigh = self.today + timedelta(days=int(cfg['bdaysrange'][-1]))
+		# На всякий случай удаляем из даты все кроме цифр и точек
 		bddate=re.compile(r'[^\d.]+').sub('', bddate.encode('utf-8').strip())
-		print bddate
-		if len(bddate)<4: return None
+		# Прекращаем проверку, если это не дата
+		if len(bddate) < 3: return None
 		# Добавляем даты др соседних годов
-		bddates = map(lambda i: '{}.{}'.format(bddate, int(self.today.strftime("%Y"))+i), (-1, 0, 1))
+		bddates=[]
+		for i in (-1, 0, 1):
+			bddates.append('{0}.{1}'.format(bddate, int(self.today.strftime("%Y")) + i))
 		# Возвращаем дату, если она находится в искомом диапазоне
 		for date in bddates:
 			date=datetime.strptime(date, '%d.%m.%Y')
-			print self.marginlow<=date and date<self.marginhigh
+			#print self.marginlow<=date and date<self.marginhigh
 			if self.marginlow<=date<self.marginhigh:
 				return date.strftime("%d.%m.%Y")
 
-	def sendbdnotice():
-		pass
+	def sendbdnotice(self, recipient, message):
+		msg = MIMEMultipart('alternative')
+		msg['Subject'] = cfg['mailsubject']
+		msg['From'] = cfg['mailsender']
+		msg['To'] = recipient
+
+		msg.attach(MIMEText(message, 'html'))
+
+		s = smtplib.SMTP(cfg['smtpserver'])
+		s.sendmail(cfg['mailsender'], recipient, msg.as_string())
+		s.quit()
 
 if __name__ == '__main__':
-	n = BDNotice()
+	BDNotice().run()
